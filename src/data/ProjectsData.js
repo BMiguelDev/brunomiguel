6b5ea7c370094e029b791950ddfc640743aeb4cb
Bruno Miguel <113399299+BMiguelDev@users.noreply.github.com>
2023-03-06 22:39:39 +0000

    Improve responsiveness for mobile portrait screens

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,16 +1,16 @@
 const ProjectsData = [
     {
         id: 1,
-        title: "Quizz Challenge App",
+        title: "Quizz Challenge",
         description: "Answer random trivia questions of different difficulty and categories",
         image: require('../assets/images/quizz-challenge-thumbnail-light.png'),
         imageDark: require('../assets/images/quizz-challenge-thumbnail-dark.png'),
-        url: "https://bmigueldepv.github.io/quizz-challenge",
+        url: "https://bmigueldev.github.io/quizz-challenge",
         codeUrl: "https://github.com/BMiguelDev/quizz-challenge"
     },
     {
         id: 2,
-        title: "Similar Recipes Network App",
+        title: "Similar Recipes Network",
         description: "Search and visualize a graph of similar recipes dynamically",
         image: require('../assets/images/sim-rec-net-thumbnail-light.png'),
         imageDark: require('../assets/images/sim-rec-net-thumbnail-dark.png'),
@@ -19,7 +19,7 @@ const ProjectsData = [
     },
     {
         id: 3,
-        title: "Task Manager App",
+        title: "Task Manager",
         description: "Manage your projects and organize, change, search, and sort your tasks!",
         image: require('../assets/images/task-manager-thumbnail-light.png'),
         imageDark: require('../assets/images/task-manager-thumbnail-dark.png'),
@@ -28,7 +28,7 @@ const ProjectsData = [
     },
     {
         id: 4,
-        title: "Wordle Clone App",
+        title: "Wordle Clone",
         description: "Play the popular game Wordle, improved to allow different word lengths and difficulties!",
         image: require('../assets/images/wordle-clone-thumbnail-light.png'),
         imageDark: require('../assets/images/wordle-clone-thumbnail-dark.png'),
@@ -37,7 +37,7 @@ const ProjectsData = [
     },
     {
         id: 5,
-        title: "Shopping Cart App",
+        title: "Shopping Cart",
         description: "Browse products and manage a shopping cart",
         image: require('../assets/images/shopping-cart-thumbnail-light.png'),
         imageDark: require('../assets/images/shopping-cart-thumbnail-light.png'),
