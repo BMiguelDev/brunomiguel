6b81c76905a74952e9056cd902a538de7f17b282
Bruno Miguel <113399299+BMiguelDev@users.noreply.github.com>
2023-10-20 02:25:14 +0100

    Improve responsiveness for mobile portrait screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { /* BrowserRouter,*/ Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import "./App.scss";
 import PageLayout from "./layouts/PageLayout";
